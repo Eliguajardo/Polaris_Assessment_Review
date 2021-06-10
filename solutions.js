a7e900048f41cd6a10467f5c4758f0f7a3a49f85
Elias Guajardo <eli.guajardo5@gmail.com>
2021-06-09 22:49:12 -0500

    solutions exersice

--- solutions.js
+++ solutions.js
@@ -1,2 +1,14 @@
-// Write your Solutions here!!
 
+
+
+function doingJavaScriptRightNow() {
+    return typeof true == "doingJavaScriptRightNow";
+}
+
+function sayHello(value) {
+    if (value == isNaN) {
+        "Hello, " + value + "!"
+    } else {
+        return "Hello, world!"
+    }
+};
